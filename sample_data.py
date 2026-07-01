65b6660f5a45cfb70e6d9233a51730784402c8f6
2vs7yn8w9v-commits <2vs7yn8w9v@privaterelay.appleid.com>
2026-07-01 19:14:24 +0100

    Create sample_data.py

---
+++ sample_data.py
@@ -0,0 +1,22 @@
+channels = [
+    {
+        "id": "kanto",
+        "title": "Kanto Adventures",
+        "description": "Ash begins his journey.",
+        "banner": "/images/kanto.jpg",
+        "stunt": False
+    }
+]
+
+episodes = [
+    {
+        "id": "ep001",
+        "channel_id": "kanto",
+        "season": 1,
+        "episode": 1,
+        "title": "Pokémon, I Choose You!",
+        "description": "Ash gets Pikachu.",
+        "thumbnail": "/images/ep001.jpg",
+        "stream_url": ""
+    }
+]
